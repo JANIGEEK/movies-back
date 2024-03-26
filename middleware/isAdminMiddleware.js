08bd2879913d47365120c6bb25eb66e24082c6d8
JANIGEEK <josep_angelo@hotmail.com>
2024-03-25 21:45:56 -0600

    Creacion de middleare de proteccion de endpoints

---
+++ middleware/isAdminMiddleware.js
@@ -0,0 +1,31 @@
+const asyncHandler = require("express-async-handler");
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+  /*const user = req.user;
+  if (user.isAdmin) {
+    next();
+    
+  } else {
+    res
+      .status(403)
+      .json({
+        message: "Forbidden: only administrators can access this endpoint",
+      });
+  }*/
+  try{
+    if(req.user.isAdmin !== 1){
+      res.status(401)
+      throw new Error("Not authorized as an admin")
+    } else{
+      next()
+    }
+  } catch(error){
+    console.log(error)
+    res.status(401)
+    throw new Error('Acceso no permitido no es un administrados')
+  }
+});
+
+
+
+module.exports = { isAdmin }
